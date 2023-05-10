9397f29fd553e76fdd5182482035a668d1b4c126
LuffyVairez <124120127+LuffyVairez@users.noreply.github.com>
2023-05-10 09:27:10 -0600

    Add files via upload

---
+++ test 2/migrations/000001_create_courses_table.down.sql
@@ -0,0 +1,2 @@
+--Filename: migrations/000001_create_course_table.down.sql
+DROP TABLE IF EXISTS courses;
---
+++ test 2/migrations/000001_create_courses_table.up.sql
@@ -0,0 +1,12 @@
+--Filename: migrations/000001_create_course_table.up.sql
+CREATE TABLE courses(
+	id bigserial PRIMARY KEY,
+    course_code text NOT NULL,
+	course_title text NOT NULL,
+	course_credit text NOT NULL,
+	version integer NOT NULL DEFAULT 1,
+	created_at timestamp(0) with time zone NOT NULL DEFAULT NOW()
+
+
+			
+);
